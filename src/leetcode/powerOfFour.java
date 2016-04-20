ff8104eea2c8d29c25b2a95168f0b1c8cda2fac6
shawn <guanx3@yahoo.com>
2016-04-19 23:28:59 -0400

    v2

--- src/leetcode/powerOfFour.java
+++ src/leetcode/powerOfFour.java
@@ -1,5 +1,29 @@
 package leetcode;
 
 public class powerOfFour {
+  
+  public static void main(String[] args) {
+    System.out.println(isPowerOfFour(5));
+  }
 
+    public static boolean isPowerOfFour(int num) {
+      //Loop
+  
+      double x = (double) num;
+      
+     while (x>=4) {
+        x = x/4;
+        System.out.println(x);
+      }
+      if (x == 1) {
+        return true;
+      } else {
+        return false;
+      }
+      
+    
+      
+
+     
+  }
 }
